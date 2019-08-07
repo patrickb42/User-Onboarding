33a49c1ec40411a9cac84120ae43c08f9f11f0d7
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-07 15:35:11 -0400

    functioning

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import NewUserForm from './components/NewUserForm';
+
 function App() {
+  // const [status, setStatus] = useState({})
+
   return (
     <div className="App">
+      <NewUserForm />
     </div>
   );
 }
--- src/components/NewUserForm.js
+++ src/components/NewUserForm.js
@@ -1,33 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
+import axios from 'axios';
 
-const NewUserForm = ({ values, errors, touched }) => {
 
+const NewUserForm = ({ values, errors, touched, status }) => {
+  const [users, setUsers] = useState([]);
+  
+  useEffect(() => {
+    status && setUsers([...users, status]);
+  }, [status]);
+
+  return (<>
+    {console.log(status)}
+    {console.log(users)}
+    <Form>
+      {touched.name && errors.name && <p>{errors.name}</p>}
+      <Field type="text" name="name" placeholder="Name" />
+      {touched.email && errors.email && <p>{errors.email}</p>}
+      <Field type="email" name="email" placeholder="Email" />
+      {touched.password && errors.password && <p>{errors.password}</p>}
+      <Field type="password" name="password" placeholder="Password" />
+      {touched.tos && errors.tos && <p>{errors.tos}</p>}
+      <label>
+        <Field type="checkbox" name="tos" checked={values.tos} />
+        Accept Terms of Service
+      </label>
+      <button type="submit">Submit</button>
+    </Form>
+    {users.length > 0
+      && users.map((user) => <p key={user.data.email}>{user.data.name}</p>)
+    }
+  </>);
 };
 
 export default withFormik({
-  // { username, password }
-  mapPropsToValues({}) {
+  mapPropsToValues({ name, email, password, tos }) {
     return {
-      // username: username || '',
-      // password: password || '',
+      name: name || '',
+      email: email || '',
+      password: password || '',
+      tos: tos || false,
     };
   },
 
-  validationSchema: Yup.object.shape({
-    /*
-     *  email: Yup.string()
-     *    .email('Email not valid')
-     *    .required('Email is required'),
-     *  password: Yup.string()
-     *    .min(8, 'Password must be 8 characters or longer')
-     *    .required('Password is required'),
-    **/
+  validationSchema: Yup.object().shape({
+    name: Yup.string()
+      .required('Name is required'),
+    email: Yup.string()
+      .email('Email not valid')
+      .required('Email is required'),
+    password: Yup.string()
+      .min(8, 'Password must be 8 characters or longer')
+      .required('Password is required'),
+    tos: Yup.bool()
+      .oneOf([true], 'You must agree to the Terms of Service'),
   }),
 
-  // { username, password }
-  handleSubmit({}) {
-    // console.log(username, password);
+  handleSubmit(values, { setStatus }) {
+    (async () => {
+      setStatus(await axios.post('https://reqres.in/api/users', values));
+    })();
   },
 })(NewUserForm);
